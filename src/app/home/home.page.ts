61c4749deabba08f6ce069dfa39a9681f4d03fb6
GianfrancoCh <gianfrancochiarizia@gmail.com>
2025-04-26 13:10:03 -0300

    Primer Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonText } from '@ionic/angular/standalone';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { Subscription } from 'rxjs';
+import { User } from '@supabase/supabase-js';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonText, IonButton, IonHeader, IonToolbar, IonTitle, IonContent, RouterModule, IonButton, CommonModule, IonButtons],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit{
+
+  user: User | null = null;
+  isLoggedIn = false;
+
+  constructor(private router: Router, private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.getCurrentUser().subscribe(user => {
+      this.user = user;
+      console.log('Usuario en home:', user);
+    });
+  };
+  
+  goToLogin() {
+    this.router.navigate(['/login']);
+  };
+  
+  goToRegister() {
+    this.router.navigate(['/register']);
+  };
+
+  logout() {
+    this.authService.signOut(); // Esto también redirige al login
+  };
+
 }
